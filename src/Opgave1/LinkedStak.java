d55ffc6e4ae833cf841fcc600b681e6268a41c08
Aleksander <aleksander.wm@gmail.com>
2017-02-05 15:05:10 +0100

    Rigtig brug og kommentarer

--- src/Opgave1/LinkedStak.java
+++ src/Opgave1/LinkedStak.java
@@ -2,7 +2,7 @@ package Opgave1;
 
 public class LinkedStak implements Stak {
 
-	
+	//Klasse til at oprette elementer til at oprette stakken.
 	private class Element
 	{
 		private Element last;
@@ -10,63 +10,58 @@ public class LinkedStak implements Stak {
 		private Element next;
 		
 	}
-
-
+	//Opretter 3 objekter af element. et sidste element, som lægges før first.
+	// et first element som indikerer først kort.
+	// et actual element, som indikerer det element der er aktuelt.
+	private Element last;
 	private Element first;
 	private Element actual;
-	private Element last;
+
 	
+	//Konstruktør til at oprette linkedstak object. 
 	public LinkedStak(){
 		first = null;
-
 	}
-	
+	//Sætter den aktuelle til at være den første.
 	private void setFirst()
 	{
 		actual = first;
 	}
-
+	//Gemmer en string værdi i det aktuelle element.
 	private void save(String e){
 		actual.x = e;
 	}
-	private String get(){
-		return actual.x;
+	//Returnere det aktuelle elements string værdi.
+	private String get(String e){
+		return e;
 	}
-
+	//Indsætter det første element i en stak, og definerer at first.last er last.
 	private void insertFirst(){
 		Element temp = new Element();
 		temp.next = first;
 		first = temp;
 		first.last = last;
 	}
+	//Indsætter et nyt element i stakken. Men holder stadig styr på det foregående element.
 	private void insertNext(){
 		Element temp = new Element();
-		temp.last = actual;
 		actual.next = temp;
+		temp.last = actual;
 		actual = temp;
 		
 	}
+	//Fjerner et element, ved at gå et skridt tilbage i stakken, og så sætte .next til null
 	private void delete(){
-		if(actual == first){
-			first = actual = null;
+		if(actual != first){
+		actual = actual.last;
+		actual.next = null;
 		}
+		else actual = first = null;
 		
-		else if(full()){
-			Element temp = new Element();
-			temp = actual.last;
-			actual = temp;
-			actual.next = null;
-		}
-		
-	}
-	private void findLast(){
-		setFirst();
-		while(actual.next != null){
-			actual = actual.next;
-		}
 	}
 	
 	@Override
+	//indsætter nyt element på stakken
 	public void push(String e) {
 		if(first == null){
 			insertFirst();
@@ -80,39 +75,38 @@ public class LinkedStak implements Stak {
 	}
 
 	@Override
+	//fjerner et element fra stakken.
 	public String pop() {
-		findLast();
 		String temp = actual.x;
 		delete();
-		if(actual != last)
-			findLast();
 		return temp;
-		
-
-		
 	}
-		
 
 	@Override
+	//Tjekker om stakken er tomt, ved at se om det første element, er null
 	public boolean empty() {
 		return (first == null);
 	}
 
 	@Override
+	//Tjekker om stakken er tom, ved at se om det næste element er null.
 	public boolean full() {
 		return (actual.next == null);
 	}
 
 	@Override
+	//Printer ud, så længe at det aktuelle element ikke er last, og går tilbage i stakken
+	//ved hjælp af .last
 	public void show() {
-		while(actual.last != null){
-			System.out.print(get() + " ");
-			Element temp = new Element();
-			temp = actual.last;
-			actual = temp;
+		if(actual != null){
+		Element temp = new Element();
+		temp = actual;
+		while(temp != last){
+			System.out.print(get(temp.x)+ " ");
+			temp = temp.last;
 		}
-		System.out.println(get());
-
+		}
+		else System.out.println("There is nothing in the stack");
 	}
 }
 	
--- src/Opgave1/StakTest.java
+++ src/Opgave1/StakTest.java
@@ -3,32 +3,32 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StakTest {
-	@Test
-	public void ArrayStakTest()
-	{
-		//Preconditions
-		ArrayStak stak = new ArrayStak();
-		//Test af at nyoprettet array er tomt.
-		assertTrue(stak.empty());
-		//pusher ting i arrayet og tjekker om arrayet er fyldt, som det burde være, da vi har lavet
-		//et "dynamisk" array
-		stak.push("Dette");
-		stak.push("er");
-		stak.push("en");
-		stak.push("mærkelig");
-		stak.push("sætning");
-		assertTrue(stak.full());
-		//vi laver herefter en show, og ser om den er lig sætningen vi forventer
-		stak.show();		
-		//Popper alt i arrayet og tjekker om det er tomt. Forventer det er det.
-		stak.pop();
-		stak.pop();
-		stak.pop();
-		stak.pop();
-		stak.pop();
-		assertTrue(stak.empty());
-		//Vi har nu testet alle metoderne og kan komme frem til at de alle virker.
-	}
+//	@Test
+//	public void ArrayStakTest()
+//	{
+//		//Preconditions
+//		ArrayStak stak = new ArrayStak();
+//		//Test af at nyoprettet array er tomt.
+//		assertTrue(stak.empty());
+//		//pusher ting i arrayet og tjekker om arrayet er fyldt, som det burde være, da vi har lavet
+//		//et "dynamisk" array
+//		stak.push("Dette");
+//		stak.push("er");
+//		stak.push("en");
+//		stak.push("mærkelig");
+//		stak.push("sætning");
+//		assertTrue(stak.full());
+//		//vi laver herefter en show, og ser om den er lig sætningen vi forventer
+//		stak.show();		
+//		//Popper alt i arrayet og tjekker om det er tomt. Forventer det er det.
+//		stak.pop();
+//		stak.pop();
+//		stak.pop();
+//		stak.pop();
+//		stak.pop();
+//		assertTrue(stak.empty());
+//		//Vi har nu testet alle metoderne og kan komme frem til at de alle virker.
+//	}
 	@Test
 	public void LinkedStakTest()
 	{
